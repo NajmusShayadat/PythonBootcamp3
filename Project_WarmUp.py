011c5c48d583104734bd3411f6de2ba50c963386
NajmusShayadat <najmus.bappy@gmail.com>
2020-06-29 00:03:57 +0600

    Udemy Chapter 08: Udemy Python 3 Bootcamp
    Topic: OOP
    
    1. Added a split_cards method in HandDeck class card to add cards in the beginning of the game.
    2. Added the round winner print output in the win_round method in HandDeck class.
    3. Added a function to print winner information of the game.
    
    Fixed space and text comment typos.

--- Project_WarmUp.py
+++ Project_WarmUp.py
@@ -22,15 +22,14 @@ class Card:
 class Deck:
 
     def __init__(self):
-
         self.all_cards = []
+
         for suit in suits:
             for rank in ranks:
                 # create a Card object and append in the deck object
                 self.all_cards.append(Card(suit, rank))
 
     def deal(self):
-
         return self.all_cards.pop()
 
     def shuffle(self):
@@ -43,9 +42,11 @@ class Deck:
 class HandDeck:
 
     def __init__(self, name):
-
         self.name = name
         self.cards = []
+
+    def split_cards(self, single_card):
+        self.cards.append(single_card)
 
     def play(self):
         return self.cards.pop(0)
@@ -61,16 +62,22 @@ class HandDeck:
 
         if isinstance(new_cards, list):
             self.cards.extend(new_cards)
+
         else:
             self.cards.append(new_cards)
 
-        # print(f"Player {self.name} wins this round")
+        print(f"Player {self.name} wins this round!\nCard balance: {len(self.cards)} cards.")
 
     def __str__(self):
         return f"Player{self.name} has {len(self.cards)} cards."
 
     def __len__(self):
         return len(self.cards)
+
+
+def winner(won, lost):
+    print(f"Player {won.name}: {len(won)} cards\nPlayer {lost.name}: {len(lost)} cards.")
+    print(f"Player {won.name} wins!")
 
 
 # Deck object instance
@@ -87,11 +94,12 @@ hand2 = HandDeck('Jose')
 # win method of HandDeck class adds card to the current list
 # deal at the beginning
 for n in range(int(len(deck) / 2)):
-    hand1.win_round(deck.deal())
-    hand2.win_round(deck.deal())
+    hand1.split_cards(deck.deal())
+    hand2.split_cards(deck.deal())
 
 game_on = True
 r = 0
+
 while game_on:
 
     r += 1
@@ -100,22 +108,7 @@ while game_on:
     # players play 1 card each to the board
     board = []
     board.append(hand1.play())
-    print(f"player 1 plays {board[-1]}")
     board.append(hand2.play())
-    print(f"player 2 plays {board[-1]}")
-
-    # If any player runs out of cards. Game exits declaring one winner.
-    if len(hand2) == 0:
-        print(f"Player {hand1.name}: {len(hand1)} cards\nPlayer {hand2.name}: {len(hand2)} cards.")
-        print(f"Player {hand1.name} wins!")
-        game_on = False
-        break
-
-    if len(hand1) == 0:
-        print(f"Player {hand1.name}: {len(hand1)} cards\nPlayer {hand2.name}: {len(hand2)} cards.")
-        print(f"Player {hand2.name} wins!")
-        game_on = False
-        break
 
     war_on = True
 
@@ -123,15 +116,35 @@ while game_on:
 
         # check hand 1 > hand 2, or hand 1 < hand 2 or hand 1 == hand 2
         # if someone wins, add the card(s) from the board cards to the winners deck
-        # while loop exits for the next round
+        # if loosing hand runs out of the card, game is over
+        # if they have enough card while loop exits for the next round
 
         # board[-1] is played by player 2
         if board[-1].value > board[-2].value:  # condition for player 2 win
             hand2.win_round(board)
+            # print(f"Player {hand1.name} has: {len(hand1)} cards.")
+            # print(f"Player {hand2.name} has: {len(hand2)} cards.")
+
+            # if Player 1 has no card, Player 1 lost the whole game
+            if len(hand1) == 0:
+                winner(hand2, hand1)
+                game_on = False
+
+                break
+
             war_on = False  # Proceed to next round
 
         elif board[-1].value < board[-2].value:  # condition for player 1 win
             hand1.win_round(board)
+            # print(len(hand1), len(hand2))
+
+            # if Player 2 has no card, Player 2 lost the whole game
+            if len(hand2) == 0:
+                winner(hand1, hand2)
+                game_on = False
+
+                break
+
             war_on = False  # Proceed to next round
 
         else:  # condition for TIE/WAR
@@ -139,44 +152,41 @@ while game_on:
 
             # check if players have enough cards to go to the war.
 
-            # if BOTH DOESN"T HAVE ENOUGH CARDS in hand.
-            if len(hand1) < war_stake and len(hand2) < war_stake:
-
+            # if BOTH DOESN'T HAVE ENOUGH CARDS in hand.
+            if len(hand1) <= war_stake + 1 and len(hand2) <= war_stake + 1:
                 # PLayer 1 less than 5 cards, Player 1 has more than Player 2
                 if len(hand1) > len(hand2):
-
-                    print(f"Player {hand1.name}: {len(hand1)} cards\nPlayer {hand2.name}: {len(hand2)} cards.")
-                    print(f"Player {hand1.name} wins!")
+                    winner(hand1, hand2)
                     game_on = False
+
                     break
 
                 # Player 2 less than 5 cards, but Player 2 has more than Player 1
                 elif len(hand1) < len(hand2):
-
-                    print(f"Player {hand1.name}: {len(hand1)} cards\nPlayer {hand2.name}: {len(hand2)} cards.")
-                    print(f"Player {hand2.name} wins!")
+                    winner(hand2, hand1)
                     game_on = False
+
                     break
 
-                # Both players have less than 5 cards and equal number of cards, game is tied and the game exits.
+                # Both players have less than 6 cards and equal number of cards, game is tied and the game exits.
                 else:
-
                     print(f"Game Tied! You both have {len(hand1)} cards in hand")
                     game_on = False
+
                     break
 
             # If only PLAYER 2 HAS LESS THAN ENOUGH CARDS in hand
-            elif len(hand2) < war_stake:
-                print(f"Player {hand1.name}: {len(hand1)} cards\nPlayer {hand2.name}: {len(hand2)} cards.")
-                print(f"Player {hand1.name} wins!")
+            elif len(hand2) <= war_stake:
+                winner(hand1, hand2)
                 game_on = False
+
                 break
 
             # If only PLAYER 1 HAS LESS THAN ENOUGH CARDS in hand
-            elif len(hand1) < war_stake:
-                print(f"Player {hand1.name}: {len(hand1)} cards\nPlayer {hand2.name}: {len(hand2)} cards.")
-                print(f"Player {hand2.name} wins!")
+            elif len(hand1) <= war_stake:
+                winner(hand2, hand1)
                 game_on = False
+
                 break
 
             else:
